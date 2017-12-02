4ac7ff9da7e54ed9fe80f774132fee3c24a450d2
Feer Aleksandr <pertik@list.ru>
2017-12-03 01:14:54 +0300

    add watch

---
+++ source/menu.js
@@ -0,0 +1,13 @@
+export default function (array, className) {
+    var menu = document.createElement("ul"),
+        listItems = '';
+
+    menu.className = className;
+
+    array.forEach(function(item) {
+        listItems += "<li>" + item + "</li>";
+    });
+
+    menu.innerHTML = listItems;
+    return menu;
+}
