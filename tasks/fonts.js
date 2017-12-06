6c7686821392a752bd1aae7247a7f055d8899c5b
Feer Aleksandr <pertik@list.ru>
2017-12-07 02:09:46 +0300

    edit fonts.js options

--- tasks/fonts.js
+++ tasks/fonts.js
@@ -1,21 +1,24 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = function () {
+module.exports = function (paths) {
   return {
     module: {
       rules: [
         {
           test: /^(?!.*\.generated\.ttf$).*\.ttf$/,
+          include: paths,
           use: ExtractTextPlugin.extract({
+            publicPath: '../',
             use: ['css-loader', 'fontface-loader']
           }),
-        }, {
+        },
+        {
           test: /\.generated.(ttf|eot|woff|woff2)$/,
           use: [
             {
               loader: 'file-loader',
               options: {
-                outputPath: '/fonts/',
+                outputPath: 'fonts/',
                 name: '[name].[ext]'
               },
             },
