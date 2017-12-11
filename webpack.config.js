d704bd7117a0e75f7ac5edc9d6769da25473338c
Feer Aleksandr <pertik@list.ru>
2017-12-11 20:46:25 +0300

    edit webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,14 +13,27 @@ const PATHS = {
   build: path.join(__dirname, 'build'),
 };
 
-module.exports = env => {
-  const dev = env === 'dev';
-  const prod = env === 'prod';
+const dev = process.env.NODE_ENV === 'dev';
+const prod = process.env.NODE_ENV === 'prod';
 
-  console.log('dev: ', dev);
-  console.log('prod: ', prod);
-
-  let plugins = [
+module.exports = {
+  entry: {
+    'about': PATHS.source + '/views/about/about.js',
+    'blog': PATHS.source + '/views/blog/blog.js',
+    'index': PATHS.source + '/views/index/index.js',
+    'works': PATHS.source + '/views/works/works.js',
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  },
+  devtool: dev ? 'inline-cheap-module-source-map' : '(none)',
+  output: {
+    path: PATHS.build,
+    filename: prod ? './js/[name].min.js' : './js/[name].js',
+  },
+  plugins: [
     new HtmlWebpackPlugin({
       filename: 'about.html',
       chunks: ['about', 'common'],
@@ -52,143 +65,136 @@ module.exports = env => {
       $: 'jquery',
       jQuery: 'jquery',
     }),
-  ];
-
-  if(prod) {
-    plugins.push(new CleanWebpackPlugin(PATHS.build));
-    plugins.push(new OptimizeCssAssetsPlugin({cssProcessorOptions:{discardComments:{removeAll: true}}}));
-    plugins.push(new UglifyJsPlugin());
-    plugins.push(new FaviconsWebpackPlugin({logo: PATHS.source + '/img/leaf.png', prefix: 'icons-[name]/'}));
-  };
-
-  return {
-    entry: {
-      'about': PATHS.source + '/views/about/about.js',
-      'blog': PATHS.source + '/views/blog/blog.js',
-      'index': PATHS.source + '/views/index/index.js',
-      'works': PATHS.source + '/views/works/works.js',
-    },
-    devServer: {
-      stats: 'errors-only',
-    },
-    devtool: dev ? 'inline-cheap-module-source-map' : '(none)',
-    output: {
-      path: PATHS.build,
-      filename: prod ? './js/[name].min.js' : './js/[name].js',
-    },
-    plugins: plugins,
-    module: {
-      rules: [
-        {
-          test: /\.pug$/,
-          loader: 'pug-loader',
-          options: {
-            pretty: true,
-          },
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true,
         },
-        {
-          test: /\.scss$/,
-          use: ExtractTextPlugin.extract({
-            publicPath: '../',
-            use: [
-              {
-                loader: 'css-loader',
-                options: { importLoaders: 1 } },
-              {
-                loader: 'postcss-loader',
-                options:{
-                  plugins: (loader) => [
-                    require('autoprefixer')({
-                      browsers: ['> 1%', 'last 2 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11']
-                    })
-                  ]
-                }
-              },
-              'sass-loader',
-            ],
-          }),
-        },
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [
-              'css-loader',
-            ]
-          }),
-        },
-        {
-          test: /\.js$/,
-          enforce: "pre",
-          exclude: /node_modules/,
-          loader: "eslint-loader",
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['babel-preset-env'],
-            },
-          },
-        },
-        {
-          test: /\.(jpe?g|png|gif)$/,
-          exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          publicPath: '../',
           use: [
             {
-              loader: 'file-loader',
-              options: {
-                name: 'images/[name].[ext]',
-              },
-            },
+              loader: 'css-loader',
+              options: { importLoaders: 1 } },
             {
-              loader: 'img-loader',
-              options: {
-                enabled: prod,
+              loader: 'postcss-loader',
+              options:{
+                plugins: (loader) => [
+                  require('autoprefixer')({
+                    browsers: ['> 1%', 'last 2 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11']
+                  })
+                ]
               }
             },
+            'sass-loader',
           ],
-        },
-        {
-          test: /\.svg$/,
+        }),
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
           use: [
-            {
-              loader: 'svg-sprite-loader'
-            },
-            {
-              loader: 'svgo-loader',
-              options: {
-                plugins: [
-                  { removeNonInheritableGroupAttrs: true },
-                  { collapseGroups: true },
-                  { removeAttrs: { attrs: '(fill|stroke)' } },
-                ],
-              },
-            },
-          ],
+            'css-loader',
+          ]
+        }),
+      },
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env'],
+          },
         },
-        {
-          test: /^(?!.*\.generated\.ttf$).*\.ttf$/,
-          use: ExtractTextPlugin.extract({
-            publicPath: '../',
-            use: ['css-loader', 'fontface-loader'],
-          }),
-        },
-        {
-          test: /\.generated.(ttf|eot|woff|woff2)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                outputPath: 'fonts/',
-                name: '[name].[ext]',
-              },
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]',
             },
-          ],
+          },
+          {
+            loader: 'img-loader',
+            options: {
+              enabled: prod,
+            }
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-sprite-loader'
+          },
+          {
+            loader: 'svgo-loader',
+            options: {
+              plugins: [
+                { removeNonInheritableGroupAttrs: true },
+                { collapseGroups: true },
+                { removeAttrs: { attrs: '(fill|stroke)' } },
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /^(?!.*\.generated\.ttf$).*\.ttf$/,
+        use: ExtractTextPlugin.extract({
+          publicPath: '../',
+          use: ['css-loader', 'fontface-loader'],
+        }),
+      },
+      {
+        test: /\.generated.(ttf|eot|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts/',
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
+if (prod) {
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new CleanWebpackPlugin(PATHS.build),
+    new OptimizeCssAssetsPlugin({
+      cssProcessorOptions:{
+        discardComments:{
+          removeAll: true,
         },
-      ],
-    },
-  };
+      },
+    }),
+    new UglifyJsPlugin(),
+    new FaviconsWebpackPlugin({
+      logo: PATHS.source + '/img/leaf.png',
+      prefix: 'icons-[name]/',
+    }),
+  ]);
 };
