c6ae66d84a7ecd0b67aae31cf24e5cebcee7a70d
Feer Aleksandr <pertik@list.ru>
2017-12-19 02:45:36 +0300

    add blog page

--- assets/views/blog/blog.js
+++ assets/views/blog/blog.js
@@ -1,4 +1,27 @@
 import 'normalize.css';
 import './blog.scss';
 
+/* COMPONENTS */
+import Header from '../../components/header';
+import Footer from '../../components/footer';
+
+/* FONTS */
+import '../../fonts/Roboto-Regular.ttf';
+import '../../fonts/Roboto-Bold.ttf';
+import '../../fonts/Sansus-Webissimo-Regular.ttf';
+
+/* ICONS */
+import '../../img/icons/bg.svg';
+
+let header = new Header();
+let footer = new Footer();
+
+let wrapper = document.querySelector('#wrapper');
+let main = document.querySelector('.main');
+
+wrapper.insertBefore(header.elem, main);
+wrapper.appendChild(footer.elem);
+
+
+
 console.log('in blog.js');
